229d73d0cd3147bd75927b3aca48d896da2191f7
‘QiuYeDx’ <qiuyedx@foxmail.com>
2023-06-19 17:20:16 +0800

    v0.1.3 testCreateQuestionnaireStepB() Finished.

--- src/main/java/com/example/selenium_test/SeleniumTestApplication.java
+++ src/main/java/com/example/selenium_test/SeleniumTestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import selenium_questionnaire.SeleniumQuestionnaire;
 import selenium_test.SeleniumTest;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class SeleniumTestApplication {
 
@@ -13,9 +15,11 @@ public class SeleniumTestApplication {
         SpringApplication.run(SeleniumTestApplication.class, args);
         SeleniumQuestionnaire seleniumQuestionnaire = new SeleniumQuestionnaire();
         WebDriver driver =  seleniumQuestionnaire.initWebDriver("/Users/baizihan/Documents/学习空间/Java/selenium_test/src/main/resources/chromedriver");
-        if(seleniumQuestionnaire.testLogin(driver) &&
-                seleniumQuestionnaire.testCreateQuestionnaireStepA(driver)){
-            System.out.println("测试流程完毕!");
+        if(     seleniumQuestionnaire.testLogin(driver) &&
+                seleniumQuestionnaire.testCreateQuestionnaireStepA(driver) &&
+                seleniumQuestionnaire.testCreateQuestionnaireStepB(driver)
+        ){
+            System.out.println(new Date() + "  测试流程完毕!");
         }
     }
 
--- src/main/java/selenium_questionnaire/SeleniumQuestionnaire.java
+++ src/main/java/selenium_questionnaire/SeleniumQuestionnaire.java
@@ -1,5 +1,6 @@
 package selenium_questionnaire;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -64,7 +65,7 @@ public class SeleniumQuestionnaire {
     }
 
     /**
-     * 模拟用户创建问卷
+     * 模拟用户创建问卷 - 步骤A
      * @param driver WebDriver对象
      * @return 是否创建成功
      */
@@ -103,6 +104,142 @@ public class SeleniumQuestionnaire {
         driver.findElement(By.cssSelector("body > div:nth-child(8) > div.datetimepicker-days > table > tbody > tr:nth-child(6) > td:nth-child(7)")).click();
         // 点击"立即创建"按钮
         driver.findElement(By.cssSelector("body > div.container > form > div:nth-child(4) > div > button")).click();
+
+        // 等待警告框出现
+        Alert alert = driver.switchTo().alert();
+        // 获取警告框的文本内容
+        String alertText = alert.getText();
+        System.out.println(date + "  警告框内容：" + alertText);
+        // 点击警告框的“确定”按钮
+        alert.accept();
+        return true;
+    }
+
+    /**
+     * 模拟用户创建问卷 - 步骤B
+     * @param driver WebDriver对象
+     * @return 是否创建成功
+     */
+    public boolean testCreateQuestionnaireStepB(WebDriver driver){
+        // 等待页面加载完成，最长等待时间为 5 秒
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("top-content")));
+
+        // 点击"单选"按钮，创建单选问题
+        driver.findElement(By.cssSelector("body > div.container > div.top > div > div:nth-child(1)")).click();
+        // 等待，确保出现单选问题编辑框
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#question0 #problemName")));
+        // 编辑单选问题题目
+        driver.findElement(By.cssSelector("#question0 #problemName")).sendKeys("您的性别是？");
+        // 点击两次"添加选项"按钮
+        driver.findElement(By.cssSelector("#question0 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question0 > div.bottom > div:nth-child(3) > button")).click();
+        // 填写选项0
+        driver.findElement(By.cssSelector("#question0 > div.bottom > div.option > #optionItem0 > #chooseTerm")).sendKeys("男");
+        // 填写选项1
+        driver.findElement(By.cssSelector("#question0 > div.bottom > div.option > #optionItem1 > #chooseTerm")).sendKeys("女");
+        // 填写选项2
+        driver.findElement(By.cssSelector("#question0 > div.bottom > div.option > #optionItem2 > #chooseTerm")).sendKeys("保密");
+        // 点击"完成编辑"，保存该单选问题
+        driver.findElement(By.cssSelector("#question0 > div.bottom > div.btn-group > #editFinish")).click();
+
+        // 点击"多选"按钮，创建多选问题
+        driver.findElement(By.cssSelector("body > div.container > div.top > div > div:nth-child(2)")).click();
+        // 等待，确保出现多选问题编辑框
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#question1 #problemName")));
+        // 编辑多选问题题目
+        driver.findElement(By.cssSelector("#question1 #problemName")).sendKeys("您会使用哪些编程语言？");
+        // 点击五次"添加选项"按钮
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div:nth-child(3) > button")).click();
+        // 填写选项0
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.option > #optionItem0 > #chooseTerm")).sendKeys("C/C++");
+        // 填写选项1
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.option > #optionItem1 > #chooseTerm")).sendKeys("Java");
+        // 填写选项2
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.option > #optionItem2 > #chooseTerm")).sendKeys("JavaScript");
+        // 填写选项3
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.option > #optionItem3 > #chooseTerm")).sendKeys("Python");
+        // 填写选项4
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.option > #optionItem4 > #chooseTerm")).sendKeys("Rust");
+        // 填写选项5
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.option > #optionItem5 > #chooseTerm")).sendKeys("其他");
+        // 点击"完成编辑"，保存该多选问题
+        driver.findElement(By.cssSelector("#question1 > div.bottom > div.btn-group > #editFinish")).click();
+
+        // 点击"填空"按钮，创建填空问题
+        driver.findElement(By.cssSelector("body > div.container > div.top > div > div:nth-child(3)")).click();
+        // 等待，确保出现填空问题编辑框
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#question2 #problemName")));
+        // 编辑填空问题题目
+        driver.findElement(By.cssSelector("#question2 #problemName")).sendKeys("您的座右铭是什么？");
+        // 点击"完成编辑"，保存该填空问题
+        driver.findElement(By.cssSelector("#question2 > div.bottom > div.btn-group > #editFinish")).click();
+
+        // 点击"矩阵"按钮，创建矩阵问题
+        driver.findElement(By.cssSelector("body > div.container > div.top > div > div:nth-child(4)")).click();
+        // 等待，确保出现矩阵问题编辑框
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#question3 #problemName")));
+        // 编辑矩阵问题题目
+        driver.findElement(By.cssSelector("#question3 #problemName")).sendKeys("您是否拥有以下兴趣爱好？");
+        // 编辑左标题
+        driver.findElement(By.cssSelector("#question3 #leftTitle")).sendKeys("运动,摄影,旅行,音乐,阅读,游戏,其他");
+        // 点击一次"添加选项"按钮
+        driver.findElement(By.cssSelector("#question3 > div.bottom > div:nth-child(5) > button")).click();
+        // 填写选项0
+        driver.findElement(By.cssSelector("#question3 > div.bottom > div.option > #optionItem0 > #chooseTerm")).sendKeys("有");
+        // 填写选项1
+        driver.findElement(By.cssSelector("#question3 > div.bottom > div.option > #optionItem1 > #chooseTerm")).sendKeys("没有");
+        // 点击"完成编辑"，保存该矩阵问题
+        driver.findElement(By.cssSelector("#question3 > div.bottom > div.btn-group > #editFinish")).click();
+
+        // 点击"量表"按钮，创建量表问题
+        driver.findElement(By.cssSelector("body > div.container > div.top > div > div:nth-child(5)")).click();
+        // 等待，确保出现量表问题编辑框
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#question4 #problemName")));
+        // 编辑量表问题题目
+        driver.findElement(By.cssSelector("#question4 #problemName")).sendKeys("您是否推荐朋友学习计算机专业？");
+        // 点击四次"添加选项"按钮
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div:nth-child(3) > button")).click();
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div:nth-child(3) > button")).click();
+        // 填写选项0
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem0 > #chooseTerm")).sendKeys("非常不推荐");
+        // 填写选项1
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem1 > #chooseTerm")).sendKeys("不推荐");
+        // 填写选项2
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem2 > #chooseTerm")).sendKeys("一般");
+        // 填写选项3
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem3 > #chooseTerm")).sendKeys("推荐");
+        // 填写选项4
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem4 > #chooseTerm")).sendKeys("非常推荐");
+        // 填写分数0
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem0 > #fraction")).sendKeys("0");
+        // 填写分数1
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem1 > #fraction")).sendKeys("1");
+        // 填写分数2
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem2 > #fraction")).sendKeys("2");
+        // 填写分数3
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem3 > #fraction")).sendKeys("3");
+        // 填写分数4
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.option > #optionItem4 > #fraction")).sendKeys("4");
+        // 点击"完成编辑"，保存该矩阵问题
+        driver.findElement(By.cssSelector("#question4 > div.bottom > div.btn-group > #editFinish")).click();
+
+        // 点击"完成编辑"，保存本问卷
+        driver.findElement(By.cssSelector("body > div.header > button.btn-success")).click();
+
+        // 等待警告框出现
+        Alert alert = driver.switchTo().alert();
+        // 获取警告框的文本内容
+        String alertText = alert.getText();
+        System.out.println(new Date() + "  警告框内容：" + alertText);
+        // 点击警告框的“确定”按钮
+        alert.accept();
         return true;
     }
 }
